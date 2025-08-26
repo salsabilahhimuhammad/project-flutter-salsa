fdbed6463b3cfb1dc5570f44f9319557adfbc8db
unknown <salsabilahhimuh@gmail.com>
2025-08-26 15:29:34 +0900

    Upload Pertama Project

---
+++ bin/project_salsa.dart
@@ -0,0 +1,5 @@
+import 'package:project_salsa/project_salsa.dart' as project_salsa;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_salsa.calculate()}!');
+}
---
+++ test/project_salsa_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_salsa/project_salsa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
